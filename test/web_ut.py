af725b26f238a0fa8b5f4330ce5e226de032f958
xj2019033 <1102421956@qq.com>
2020-02-21 16:20:14 +0800

    change options

--- test/web_ut.py
+++ test/web_ut.py
@@ -29,7 +29,7 @@ class ISelenium(unittest.TestCase):
             using_headless = None
             print('没有配置环境变量 using_headless, 按照有界面方式运行自动化测试')
 
-        chrome_options = webdriver.ChromeOptions()
+        chrome_options = Options()
         if using_headless is not None and using_headless.lower() == 'true':
             print('使用无界面方式运行')
             chrome_options.add_argument("--headless")
